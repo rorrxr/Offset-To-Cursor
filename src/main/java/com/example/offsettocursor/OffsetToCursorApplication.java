79258747c00c33a47fef4e9663ee0c1cdfb1b106
rorrxr <rorrxr@naver.com>
2025-07-10 12:56:47 +0900

    chore : 스프링 프로젝트 초기 설정

---
+++ src/main/java/com/example/offsettocursor/OffsetToCursorApplication.java
@@ -0,0 +1,13 @@
+package com.example.offsettocursor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffsetToCursorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OffsetToCursorApplication.class, args);
+    }
+
+}
